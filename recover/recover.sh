e8fe52591e37d1670189d0214bff4ba572b6d419
22RQAQ <1142556134@qq.com>
2024-09-24 14:10:27 +0800

    iteration改为400，测试

--- recover/recover.sh
+++ recover/recover.sh
@@ -3,6 +3,6 @@ python data_synthesis.py \
     --exp-name "rn18_bn0.01_[4K]_x_l2_x_tv.crop" \
     --batch-size 100 \
     --lr 0.25 \
-    --iteration 4000 \
+    --iteration 400 \
     --l2-scale 0 --tv-l2 0 --r-bn 0.01 \
     --verifier --store-best-images
